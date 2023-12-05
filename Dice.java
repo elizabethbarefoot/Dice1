9c2f059e432733bac15b79fd7cf3bf38b1eb35e3
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2023-12-05 11:38:20 -0800

    initial commit

---
+++ Dice.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class Dice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Dice
+{
+    
+    Die6 die1;
+    Die6 die2;
+
+    /**
+     * Constructor for objects of class Dice
+     */
+    public Dice()
+    { 
+        die1 = new Die6();
+        die2 = new Die6();
+    }
+    
+    public void roll() {
+        die1.roll();
+        die2.roll();
+    }
+    
+    
+    public int getValue() {
+        return die1.getValue() + die2.getValue();
+    }
+    
+    public int rollAndGetValue() {
+        roll();
+        return getValue();
+    }
+
+}
